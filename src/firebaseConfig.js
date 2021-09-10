d6fa5b184d02d0ad710203a8bf2993738569d21f
omid345 <omid.345@gmail.com>
2021-09-09 23:18:15 -0400

    added firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBUQJbwoFq5uAw3zeFDF-TP-33mlP8ficg",
+  authDomain: "afghan-diasporas.firebaseapp.com",
+  projectId: "afghan-diasporas",
+  storageBucket: "afghan-diasporas.appspot.com",
+  messagingSenderId: "123972608524",
+  appId: "1:123972608524:web:be6f649dc10b5dceae0429",
+  measurementId: "G-FZH919R8KC"
+};
